e13edfbcf8a650a93c275692cc9d6ef23ff46202
Vitor Brocker <vitorbrocker123@gmail.com>
2026-03-26 15:30:20 -0300

    first commit

---
+++ src/main/java/com/vitor/pdfapi/PdfApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.pdfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfApiApplication.class, args);
+	}
+
+}
